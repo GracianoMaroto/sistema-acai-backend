444e8f73bce54ddf5eeea2f9076a41e970ed2592
Graciano Maroto <gracianoml07@gmail.com>
2026-03-10 12:03:25 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'https://sistema-acainagarrafavdc.netlify.app/',
-      'http://localhost:9001',
-    ],
+    origin: true,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
