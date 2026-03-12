6852fc6a1a5e5b4bae4d68d2a1cbcffa5c1332d5
Graciano Maroto <gracianoml07@gmail.com>
2026-03-12 09:04:02 -0300

    neightborhood added

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,9 +27,9 @@ async function main() {
     });
 
     const sellerRole = await prisma.role.upsert({
-      where: { name: 'SELLER' },
+      where: { name: 'VENDEDOR' },
       update: {},
-      create: { name: 'SELLER' },
+      create: { name: 'VENDEDOR' },
     });
 
     // USERS
@@ -47,11 +47,11 @@ async function main() {
     });
 
     const sellerUser = await prisma.user.upsert({
-      where: { email: 'seller@acai.com' },
+      where: { email: 'vendedor@acai.com' },
       update: {},
       create: {
-        name: 'Seller',
-        email: 'seller@acai.com',
+        name: 'Vendedor',
+        email: 'vendedor@acai.com',
         passwordHash: password,
         roleId: sellerRole.id,
       },
@@ -152,32 +152,17 @@ async function main() {
 
     // Clientes
     const cliente1 = await prisma.customer.upsert({
-      where: { email: 'joao@email.com' },
+      where: { email: 'clientegenerico@email.com' },
       update: {},
       create: {
-        name: 'João Silva',
-        phone: '11999999999',
-        email: 'joao@email.com',
+        name: 'Cliente Genérico',
+        phone: '(77)99999-9999',
+        email: 'clientegenerico@email.com',
         street: 'Rua A',
         number: '123',
-        city: 'São Paulo',
-        state: 'SP',
-        zipCode: '01000000',
-      },
-    });
-
-    const cliente2 = await prisma.customer.upsert({
-      where: { email: 'maria@email.com' },
-      update: {},
-      create: {
-        name: 'Maria Souza',
-        phone: '11988888888',
-        email: 'maria@email.com',
-        street: 'Rua B',
-        number: '456',
-        city: 'São Paulo',
-        state: 'SP',
-        zipCode: '02000000',
+        city: 'Vitória da Conquista',
+        state: 'BA',
+        zipCode: '45000-000',
       },
     });
 
@@ -186,7 +171,7 @@ async function main() {
       where: { name: 'Street' },
       update: {},
       create: {
-        name: 'Street',
+        name: 'Rua',
         modifierType: 'DISCOUNT',
         priceModifier: new Prisma.Decimal(0),
       },
@@ -208,7 +193,7 @@ async function main() {
       create: {
         name: 'Ifood',
         modifierType: 'MARKUP',
-        priceModifier: new Prisma.Decimal(10), // 10% de markup
+        priceModifier: new Prisma.Decimal(10),
       },
     });
 
@@ -232,7 +217,6 @@ async function main() {
       },
     });
 
-    // PRODUTO AÇAÍ
     const acai = await prisma.product.create({
       data: {
         name: 'Açaí na Garrafa 300ml',
@@ -254,7 +238,11 @@ async function main() {
       data: { name: 'Morango', stockQuantity: 16, productId: acai.id },
     });
 
-    const variants = [tradicional, maracuja, morango];
+    const nutella = await prisma.productVariant.create({
+      data: { name: 'Nutella', stockQuantity: 16, productId: acai.id },
+    });
+
+    const variants = [tradicional, maracuja, morango, nutella];
 
     for (const variant of variants) {
       await prisma.stockMovement.create({
@@ -268,26 +256,25 @@ async function main() {
     }
     // PREÇOS AÇAÍ
     const prices = [
-      { variant: tradicional, rua: [20, 7.5], ifood: [21.9, 13.3] },
-      { variant: maracuja, rua: [23, 8.91], ifood: [23.9, 15.2] },
-      { variant: morango, rua: [23, 9.47], ifood: [24.9, 16] },
+      { variant: tradicional, street: [20, 6.69], ifood: [21.9, 13.3] },
+      { variant: maracuja, street: [23, 7.25], ifood: [23.9, 15.2] },
+      { variant: morango, street: [23, 8.59], ifood: [24.9, 16] },
+      { variant: nutella, street: [23, 8.59], ifood: [24.9, 16] },
     ];
     const channels = [street, horto, instagram, whatsapp];
 
     for (const item of prices) {
-      // Preços iguais para todos os canais (exceto iFood)
       for (const channel of channels) {
         await prisma.productPrice.create({
           data: {
             productVariantId: item.variant.id,
             saleChannelId: channel.id,
-            price: new Prisma.Decimal(item.rua[0]),
-            cost: new Prisma.Decimal(item.rua[1]),
+            price: new Prisma.Decimal(item.street[0]),
+            cost: new Prisma.Decimal(item.street[1]),
           },
         });
       }
 
-      // Preço específico do iFood
       await prisma.productPrice.create({
         data: {
           productVariantId: item.variant.id,
@@ -297,120 +284,6 @@ async function main() {
         },
       });
     }
-    // Orders
-    const orderPending = await prisma.order.create({
-      data: {
-        totalAmount: new Prisma.Decimal(40),
-        totalCost: new Prisma.Decimal(15),
-        totalProfit: new Prisma.Decimal(25),
-        sellerId: adminUser.id,
-        customerId: cliente1.id,
-        orderStatusId: pendingStatus.id,
-        paymentStatusId: paymentPending.id,
-        deliveryStatusId: deliveryPending.id,
-        saleChannelId: street.id,
-      },
-    });
-    await prisma.orderItem.create({
-      data: {
-        orderId: orderPending.id,
-        productVariantId: tradicional.id,
-        quantity: 2,
-        unitPrice: new Prisma.Decimal(20),
-        unitCost: new Prisma.Decimal(7.5),
-      },
-    });
-    await prisma.stockMovement.create({
-      data: {
-        type: 'OUT',
-        quantity: 2,
-        productVariantId: tradicional.id,
-        orderId: orderPending.id,
-        reason: 'Venda',
-      },
-    });
-    await prisma.productVariant.update({
-      where: { id: tradicional.id },
-      data: {
-        stockQuantity: { decrement: 2 },
-      },
-    });
-    const orderInCourse = await prisma.order.create({
-      data: {
-        totalAmount: new Prisma.Decimal(48),
-        totalCost: new Prisma.Decimal(20),
-        totalProfit: new Prisma.Decimal(28),
-        sellerId: adminUser.id,
-        customerId: cliente2.id,
-        orderStatusId: inCourseStatus.id,
-        paymentStatusId: paymentPending.id,
-        deliveryStatusId: deliveryInCourse.id,
-        saleChannelId: ifood.id,
-      },
-    });
-    await prisma.payment.create({
-      data: {
-        amount: new Prisma.Decimal(30),
-        orderId: orderInCourse.id,
-        paymentMethodId: cashPayment.id,
-      },
-    });
-    const orderFinalized = await prisma.order.create({
-      data: {
-        totalAmount: new Prisma.Decimal(70),
-        totalCost: new Prisma.Decimal(30),
-        totalProfit: new Prisma.Decimal(40),
-        sellerId: sellerUser.id,
-        customerId: cliente1.id,
-        orderStatusId: finalizedStatus.id,
-        paymentStatusId: paymentPaid.id,
-        deliveryStatusId: deliveryFinalized.id,
-        saleChannelId: whatsapp.id,
-      },
-    });
-    await prisma.order.create({
-      data: {
-        totalAmount: new Prisma.Decimal(50),
-        totalCost: new Prisma.Decimal(20),
-        totalProfit: new Prisma.Decimal(30),
-        sellerId: sellerUser.id,
-        customerId: cliente2.id,
-        orderStatusId: canceledStatus.id,
-        paymentStatusId: paymentPending.id,
-        deliveryStatusId: deliveryPending.id,
-        saleChannelId: instagram.id,
-      },
-    });
-    // Payment
-    await prisma.payment.create({
-      data: {
-        amount: new Prisma.Decimal(70),
-        orderId: orderFinalized.id,
-        paymentMethodId: pixPayment.id,
-      },
-    });
-    await prisma.payment.create({
-      data: {
-        amount: new Prisma.Decimal(40),
-        orderId: orderPending.id,
-        paymentMethodId: creditPayment.id,
-      },
-    });
-    await prisma.payment.create({
-      data: {
-        amount: new Prisma.Decimal(18),
-        orderId: orderInCourse.id,
-        paymentMethodId: debitPayment.id,
-      },
-    });
-    await prisma.payment.create({
-      data: {
-        amount: new Prisma.Decimal(30),
-        orderId: orderInCourse.id,
-        paymentMethodId: ifoodPayment.id,
-      },
-    });
-
     console.log('✅ Seed finalizado com sucesso!');
   } catch (e) {
     console.error('Erro durante o seeding:', e);
