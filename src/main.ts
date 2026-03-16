3d231f3f821e006150d99de91ebe59e6308efe18
Graciano Maroto <gracianoml07@gmail.com>
2026-03-16 09:01:50 -0300

    testando

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
-  await app.listen(process.env.PORT || 3000);
-  console.log('Olá Porta', process.env.PORT);
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,5 +17,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  await app.listen(process.env.PORT || 3000);
+  console.log('Olá Porta', process.env.PORT);
 }
 bootstrap();
